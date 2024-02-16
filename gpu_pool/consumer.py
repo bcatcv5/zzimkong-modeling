5b2d8b68aa5834cd21ef0143093451619233178b
cmj5064 <duttu5064@naver.com>
2024-02-16 12:25:01 +0000

    feat: complete gpu pool test

--- gpu_pool/consumer.py
+++ gpu_pool/consumer.py
@@ -5,6 +5,7 @@ from utils.time_ckecker import sleep
 import env
 from logger import get_logger
 import git
+import sys
 
 SLEEP_TIME: Final = 0.5
 CONFIG_INDEX: Final = 1
@@ -19,43 +20,75 @@ def handle_receive_process() -> None:
 
     while True:
         logger.info("메세지를 대기하는 중입니다.")
-        train_process()
+        listen_message()
         sleep(SLEEP_TIME)
 
+def listen_message():
+    confs = handler.pop_message()
 
-def train_process() -> None:
-    """큐에서 메세지를 추출하여 모델 학습 수행"""
-    config: bytes = handler.pop_message()
+    for c in confs[::-1]:
+        c = c.decode(DECODE_FORMAT) # str
+        if "space" in c:                    # NOTE 4090
+            config = handler.get(c)
+            return train_space_process(config)
+        if "furniture" in c:                # NOTE: 4090 이외 서버, 4090은 space 우선 furniture 다음으로 수정할 것
+            config = handler.get(c)
+            return train_furniture_process(config)
+
+def train_space_process(config) -> None:
+    """큐에서 메세지를 추출하여 공간 모델 학습 수행"""
+    # config: bytes = handler.pop_message()
+    # print(config)
 
     if config is not None:
-        command = construct_config(config)
-
-        logger.info("학습 중 입니다.")
-        git_synchronize()
+        command = construct_config(config, env.TRAIN_SPACE_PATH)
+        # logger.info("학습 중 입니다.")
+        # git_synchronize()
 
         train_log = train(command)
-        if train_log.stderr:
-            logger.info("학습 중 에러 발생 log를 확인하세요.")
-            logger.error(train_log.stderr)
-            logger.info(train_log.stdout)
-        else:
-            logger.info("학습 완료입니다.")
+        print(train_log)
+        sys.exit(1)
+        # if train_log.stderr:
+        #     logger.info("학습 중 에러 발생 log를 확인하세요.")
+        #     logger.error(train_log.stderr)
+        #     logger.info(train_log.stdout)
+        # else:
+        #     logger.info("학습 완료입니다.")
 
 
-def git_synchronize():
-    """원격에 배포된 코드 동기화"""
-    repo = git.Repo.init(path=env.ROOT_PATH)
-    repo.git.checkout(WORKING_BRANCH_NAME)
-    repo.remotes.origin.pull()
+def train_furniture_process(config) -> None:
+    """큐에서 메세지를 추출하여 가구 모델 학습 수행"""
+
+    if config is not None:
+        command = construct_config(config, env.TRAIN_FURNITURE_PATH)
+
+        # logger.info("학습 중 입니다.")
+        # # git_synchronize()
+
+        # train_log = train(command)
+        # if train_log.stderr:
+        #     logger.info("학습 중 에러 발생 log를 확인하세요.")
+        #     logger.error(train_log.stderr)
+        #     logger.info(train_log.stdout)
+        # else:
+        #     logger.info("학습 완료입니다.")
+
+
+# def git_synchronize():
+#     """원격에 배포된 코드 동기화"""
+#     repo = git.Repo.init(path=env.ROOT_PATH)
+#     repo.git.checkout(WORKING_BRANCH_NAME)
+#     repo.remotes.origin.pull()
 
 
 def convert_to_string(config) -> str:
-    byte_config: bytes = config[CONFIG_INDEX]
-    decoded_config: str = byte_config.decode(DECODE_FORMAT)
+    # byte_config: bytes = config[CONFIG_INDEX]
+    # decoded_config: str = byte_config.decode(DECODE_FORMAT)
+    decoded_config: str = config.decode(DECODE_FORMAT)
     return decoded_config
 
 
-def construct_config(config) -> str:
+def construct_config(config, train_path) -> str:
     """메세지 추출 후 모델 학습 포멧에 맞게 조정
 
     Args:
@@ -65,8 +98,11 @@ def construct_config(config) -> str:
         _type_: 모델 학습 sub process 포멧
     """
     decoded_config = convert_to_string(config)
-    return f"python {env.ROOT_PATH}/{env.TRAIN_PATH} -dc '{decoded_config}'"
 
+    print(f"python {train_path} -dc '{decoded_config}'")
+    return f"python {train_path} -dc '{decoded_config}'"
+
+    # python train.py -dc '{"input": "input.txt"}' 
 
 if __name__ == "__main__":
     handle_receive_process()
--- gpu_pool/support/queue_handler.py
+++ gpu_pool/support/queue_handler.py
@@ -1,6 +1,7 @@
 from .queue_data_source import QueueDataSource
 import json
-
+from typing import Final
+DECODE_FORMAT: Final = "utf-8"
 
 class QueueHandler:
     """redis 메시지 큐 적재, 추출 핸들러"""
@@ -8,8 +9,28 @@ class QueueHandler:
     def __init__(self) -> None:
         self.conn = QueueDataSource().getConnection()
 
-    def pop_message(self) -> dict:
-        return self.conn.blpop("config", timeout=0)
+    def exists(self, str):
+        return self.conn.exists(str)
 
-    def push_message(self, config):
-        self.conn.rpush("config", json.dumps(config))
+    def get(self, str):
+        return self.conn.get(str)
+
+    def pop_message(self) -> dict:
+        # return self.conn.blpop("config", timeout=0)
+        # return self.conn.get("config")
+        confs = self.conn.keys("*")[-5:]
+        return confs
+    
+    def push_counter(self, config):
+        # self.conn.rpush("config", json.dumps(config))
+        self.conn.set("counter", json.dumps(config))
+
+    def push_message(self, config, counter):
+        # self.conn.rpush("config", json.dumps(config))
+
+        if config["objectType"]:
+            n = counter["furniture"]
+            self.conn.set(f"furniture{n}", json.dumps(config))
+        else:
+            n = counter["space"]
+            self.conn.set(f"space{n}", json.dumps(config))
