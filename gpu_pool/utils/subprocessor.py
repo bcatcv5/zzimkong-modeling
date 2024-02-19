222f73251c28f47904d49833c87af2da2d98f66a
cmj5064 <duttu5064@naver.com>
2024-02-19 11:55:52 +0000

    feat: subprocess 실행 시 stdout 실시간 출력하도록 수정

--- gpu_pool/utils/subprocessor.py
+++ gpu_pool/utils/subprocessor.py
@@ -2,11 +2,12 @@ import subprocess as sp
 
 
 def train(command) -> sp.CompletedProcess[str]:
-    return sp.run(command, capture_output=True, text=True, shell=True)
+    # return sp.run(command, capture_output=True, text=True, shell=True)
+    return sp.run(command, capture_output=False, text=True, shell=True)
 
 
 def handle_setup(command) -> sp.CompletedProcess[str]:
-    process = sp.Popen(command, stdout=sp.PIPE, stderr=sp.PIPE, shell=True)
+    process = sp.Popen(command, stdout=sp.PIPE, stderr=sp.PIPE, shell=True, universal_newlines=True)
 
     while True:
         output = process.stdout.readline().decode()
